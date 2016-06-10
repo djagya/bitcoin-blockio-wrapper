e3a79a69d40762a9088bb484e38515d9240c7cd0
danil zakablukovskii <danil.kabluk@gmail.com>
2016-06-11 00:41:09 +0300

    fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ $(function () {
             $receiverInput = $(this).parents('form').find('.js-total-receiver'),
             receiverValue;
 
-        receiverValue = value - transactionFee * 2 - (value / 100 * feePercent);
+        receiverValue = value - transactionFee * 2 - Math.min((value / 100 * feePercent), transactionFee);
 
         value = Number((value).toFixed(8));
         receiverValue = Number((receiverValue).toFixed(8));
